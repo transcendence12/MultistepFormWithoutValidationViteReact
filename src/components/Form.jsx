fae36206b705453607f8194df5b4f21fb9d5f38c
Gosia Krawczuk <gosiek.krawczuk@gmail.com>
2023-07-30 12:10:13 +0200

    unnecessary imports removed; page display functionnality added

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,21 @@
-import React, { useState } from "react";
+import { useState } from "react";
+import SignUpInfo from "./SignUpInfo";
+import PersonalInfo from "./PersonalInfo";
+import OtherInfo from "./OtherInfo";
 
 function Form() {
   const [page, setPage] = useState(0);
   const FormTitles = ["Sign Up", "Personal Info", "Other"];
+
+  const pageDisplay = () => {
+    if(page === 0){
+      return <SignUpInfo />
+    } else if(page === 1){
+      return <PersonalInfo />
+    } else {
+      return <OtherInfo />
+    }
+  }
 
   return (
     <div className="form">
@@ -11,7 +24,7 @@ function Form() {
         <div className="header">
           <h1>{FormTitles[page]}</h1>
         </div>
-        <div className="body"></div>
+        <div className="body">{pageDisplay()}</div>
         <div className="footer">
           <button
             disabled={page == 0}
@@ -24,7 +37,7 @@ function Form() {
           <button
             disabled={page == FormTitles.length - 1}
             onClick={() => {
-              setPage((currPage) => currPage + 1);
+              setPage((currentPage) => currentPage + 1);
             }}
           >
             Next
